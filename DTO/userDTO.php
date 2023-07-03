5a968e8bb68fb692db84be52ee508596a7a47876
AntoineM314 <marcouxantoine1@gmail.com>
2023-07-03 16:09:36 +0200

    Created DTOs

---
+++ DTO/userDTO.php
@@ -0,0 +1,96 @@
+<?php
+class userDTO {
+    private $idUser;
+    private $pseudo;
+    private $email;
+    private $pass;
+    private $photo;
+    private $birthdate;
+    private $status;
+    private $description;
+    private $isAdmin;
+    private $createdAt;
+
+    // Getters and setters for all fields
+
+    public function getIdUser() {
+        return $this->idUser;
+    }
+
+    public function setIdUser($idUser) {
+        $this->idUser = $idUser;
+    }
+
+    public function getPseudo() {
+        return $this->pseudo;
+    }
+
+    public function setPseudo($pseudo) {
+        $this->pseudo = $pseudo;
+    }
+
+    public function getEmail() {
+        return $this->email;
+    }
+
+    public function setEmail($email) {
+        $this->email = $email;
+    }
+
+    public function getPass() {
+        return $this->pass;
+    }
+
+    public function setPass($pass) {
+        $this->pass = $pass;
+    }
+
+    public function getPhoto() {
+        return $this->photo;
+    }
+
+    public function setPhoto($photo) {
+        $this->photo = $photo;
+    }
+
+    public function getBirthdate() {
+        return $this->birthdate;
+    }
+
+    public function setBirthdate($birthdate) {
+        $this->birthdate = $birthdate;
+    }
+
+    public function getStatus() {
+        return $this->status;
+    }
+
+    public function setStatus($status) {
+        $this->status = $status;
+    }
+
+    public function getDescription() {
+        return $this->description;
+    }
+
+    public function setDescription($description) {
+        $this->description = $description;
+    }
+
+    public function getIsAdmin() {
+        return $this->isAdmin;
+    }
+
+    public function setIsAdmin($isAdmin) {
+        $this->isAdmin = $isAdmin;
+    }
+
+    public function getCreatedAt() {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt($createdAt) {
+        $this->createdAt = $createdAt;
+    }
+}
+?>
